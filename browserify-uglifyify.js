d54cd569c4277f7116ad7e56eb4f6b4d95ef6b78
bellbind <bellbind@gmail.com>
2017-11-21 19:43:51 +0900

    cleanup browserify script

--- browserify-uglifyify.js
+++ browserify-uglifyify.js
@@ -1,13 +1,12 @@
-
-const browserify = require('browserify');
-const b = browserify('index.es6.js', {debug: true, standalone: "default"});
-b.transform('uglifyify', {
-    global: true,
-    keep_fnames: true,
-    mangle: {
-    //keep_classnames: true,
-        reserved: ['ByteLengthQueuingStrategy', 'CountQueuingStrategy']
-    }
+require('browserify')('index.es6.js', {
+  debug: true,
+  standalone: 'default',
+}).transform('uglifyify', {
+  global: true,
+  keep_fnames: true,
+  mangle: {
+    reserved: ['ByteLengthQueuingStrategy', 'CountQueuingStrategy'],
+  },
 }).transform('babelify', {
-    plugins: ['babel-plugin-unassert']
+  plugins: ['babel-plugin-unassert'],
 }).bundle().pipe(process.stdout);
