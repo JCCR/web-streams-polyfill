f1550a58fef55dc62b8ee09e9bd20ee55f012a86
bellbind <bellbind@gmail.com>
2017-11-22 16:19:56 +0900

    Pass all tests with the babel presets
    - Split bundles for modern browsers and for traditional IE11
        - because of `web-platform-tests` passed or failed
            - passing all tests required keeping `class` syntax (ES5 based codes always fail the tests)
        - `polyfill.js`: for node.js with presets-env with "node": "current" in `.babelrc`
        - `polyfill.min.js`: for recent versions of the modern browsers (Chrome, Firefox, Safari, Edge)
        - `polyfill-ie11.min.js`: for IE11 (it is not fully compatible with the Streams specification)

--- generate-bundle-tests.js
+++ generate-bundle-tests.js
@@ -1,19 +1,26 @@
 const fs = require('fs');
 const readline = require('readline');
 
-//const bundle = `require('../../dist/polyfill.js');`;
-const bundle = `require('../../dist/polyfill.min.js');`;
+//const bundle = `../../dist/polyfill.js`;
+// `class`-syntax transpiled code fails "should be a method" tests
+//const bundle = `../../dist/polyfill-ie11.min.js`;
+const bundle = `../../dist/polyfill.min.js`;
+
+const bundled = [
+  'ReadableStream',
+  'WritableStream',
+  'TransformStream',
+  `ByteLengthQueuingStrategy`,
+  `CountQueuingStrategy`,
+];
+const matcher = RegExp(`^const (?:{ )?(${bundled.join('|')})(?: })? = require`);
 
 readline.createInterface({
-    input: fs.createReadStream(`run-web-platform-tests.js`),
-    output: fs.createWriteStream(`run-web-platform-tests-on-bundle.js`),
-    terminal: false,
+  input: fs.createReadStream(`run-web-platform-tests.js`),
+  output: fs.createWriteStream(`run-web-platform-tests-on-bundle.js`),
+  terminal: false,
 }).on('line', function (line) {
-    const m1 = line.match(
-            /^(const { (?:Readable|Writable|Transform)Stream } = )/);
-    const m2 = line.match(/^const ((?:ByteLength|Count)QueuingStrategy) = /);
-    const out = m1 ? `${m1[1]}${bundle}` :
-          m2 ? `const { ${m2[1]} } = ${bundle}` : line;
-    this.output.write(`${out}\n`);
+  const m = line.match(matcher);
+  const out = m ? `const { ${m[1]} } = require('${bundle}');` : line;
+  this.output.write(`${out}\n`);
 });
-
