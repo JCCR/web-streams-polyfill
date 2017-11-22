5e1bfc4aadd8210953e5b3c3c5b1a8052a345cec
bellbind <bellbind@gmail.com>
2017-11-22 16:44:04 +0900

    use single quote instead of backquote

--- generate-bundle-tests.js
+++ generate-bundle-tests.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
 const readline = require('readline');
 
-//const bundle = `../../dist/polyfill.js`;
+//const bundle = '../../dist/polyfill.js';
 // `class`-syntax transpiled code fails "should be a method" tests
-//const bundle = `../../dist/polyfill-ie11.min.js`;
-const bundle = `../../dist/polyfill.min.js`;
+//const bundle = '../../dist/polyfill-ie11.min.js';
+const bundle = '../../dist/polyfill.min.js';
 
 const bundled = [
   'ReadableStream',
   'WritableStream',
   'TransformStream',
-  `ByteLengthQueuingStrategy`,
-  `CountQueuingStrategy`,
+  'ByteLengthQueuingStrategy',
+  'CountQueuingStrategy',
 ];
 const matcher = RegExp(`^const (?:{ )?(${bundled.join('|')})(?: })? = require`);
 
 readline.createInterface({
-  input: fs.createReadStream(`run-web-platform-tests.js`),
-  output: fs.createWriteStream(`run-web-platform-tests-on-bundle.js`),
+  input: fs.createReadStream('run-web-platform-tests.js'),
+  output: fs.createWriteStream('run-web-platform-tests-on-bundle.js'),
   terminal: false,
 }).on('line', function (line) {
   const m = line.match(matcher);
